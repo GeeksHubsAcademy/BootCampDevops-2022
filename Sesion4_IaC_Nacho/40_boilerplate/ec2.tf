71e4900c5b10474eaef31657b0d6e55514718551
Nacho Carretero <nacho@sketch.com>
2022-10-20 10:45:49 +0200

    Web-server Packer solution

--- Sesion4_IaC_Nacho/40_boilerplate/ec2.tf
+++ Sesion4_IaC_Nacho/40_boilerplate/ec2.tf
@@ -8,13 +8,23 @@ data "aws_ami" "nginx" {
   }
 }
 
+data "aws_ami" "self_nginx" {
+  owners      = ["self"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["webserver_*"]
+  }
+}
+
 resource "aws_instance" "web_server" {
-  ami                         = data.aws_ami.nginx.id
+  ami                         = data.aws_ami.self_nginx.id
   instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnet.selected.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [data.aws_security_group.allow_traffic.id]
-  user_data                   = file("../41_cloud-admin/web-server.yml") # Cloud-admin only
+  # user_data                   = file("../41_cloud-admin/web-server.yml") # Cloud-admin only
 
   tags = {
     Name = "web_server"
